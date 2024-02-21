dfc158287772edda8f6bf3341fa15cda8bb8abc5
Berk Koşal <kosalnberk@gmail.com>
2024-02-21 03:32:35 +0300

    Update UserLogin.jsx

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -9,14 +9,19 @@ export default function UserLogin({ }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await NetworkManager.post("/token/", { email, password });
-      console.log('Access Token: ' + response.data.access);
-    } catch (error) {
-      console.log(`Error: ${error.message}`);
-    }
+    // try {
+    //   const response = await NetworkManager.post("/token/", { email, password });
+    //   console.log('Access Token: ' + response.data.access);
+    // } catch (error) {
+    //   console.log(`Error: ${error.message}`);
+    // }
+    console.log(email, password);
+    setEmail('');
+    setPassword('');
+    setIsAuthenticated(true);
 
   }
 
@@ -38,66 +43,68 @@ export default function UserLogin({ }) {
   }
 
   return (
+    <>
+      {isAuthenticated ? (
+        <>
+          <h1>You are logged in.</h1>
+          <Button sx={{ mt: '2rem' }}
+            onClick={handleEnvironmentsClick}
+            variant="outlined">
+            Get Environments
+          </Button>
 
-    !isAuthenticated ?
+          <Button
+            sx={{ mt: '2rem' }}
+            onClick={handleTargetsClick}
+            variant="outlined">
+            Get Targets
+          </Button>
+        </>
+      ) : (
+        <Box display="flex" justifyContent="center" sx={{ minHeight: "100vh", bgcolor: "#41729F" }}>
+          <Stack
+            mt="3rem"
+            justifyContent="center"
+            alignItems="center"
+            width="600px"
+            height="600px"
+            bgcolor="#C3E0E5"
+            borderRadius="10%">
 
-      <Box display="flex" justifyContent="center" sx={{ minHeight: "100vh", bgcolor: "#41729F" }}>
-        <Stack
-          mt="3rem"
-          justifyContent="center"
-          alignItems="center"
-          width="600px"
-          height="600px"
-          bgcolor="#C3E0E5"
-          borderRadius="10%">
+            <h1>Sign In</h1>
+            <form style={{ display: "flex", flexDirection: "column", justifyContent: "center" }} onSubmit={handleSubmit} noValidate autoComplete='off'>
+              <Stack direction="row" sx={{ mt: '2rem' }} spacing={5}>
+                <TextField
+                  required
+                  id="outlined-basic"
+                  label="E-Mail"
+                  variant="outlined"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                  required
+                  id="outlined-password-input"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  color='secondary'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)} />
+              </Stack>
+              <Button
+                sx={{ mt: '2rem', bgcolor: "#5885AF", }}
+                variant="contained"
+                type="submit"
+              >Submit
+              </Button>
+            </form>
 
-          <h1>Sign In</h1>
-          <form style={{ display: "flex", flexDirection: "column", justifyContent: "center" }} onSubmit={handleSubmit} noValidate autoComplete='off'>
-            <Stack direction="row" sx={{ mt: '2rem' }} spacing={5}>
-              <TextField
-                required
-                id="outlined-basic"
-                label="E-Mail"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                required
-                id="outlined-password-input"
-                label="Password"
-                variant="outlined"
-                type="password"
-                color='secondary'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)} />
-            </Stack>
-            <Button
-              sx={{ mt: '2rem', bgcolor: "#5885AF", }}
-              variant="contained"
-              type="submit"
-            >Submit
-            </Button>
-          </form>
-
-        </Stack>
+          </Stack>
 
 
-      </Box>
-
-      : <>
-        <Button sx={{ mt: '2rem' }}
-          onClick={handleEnvironmentsClick}
-          variant="outlined">
-          Get Environments
-        </Button>
-
-        <Button
-          sx={{ mt: '2rem' }}
-          onClick={handleTargetsClick}
-          variant="outlined">
-          Get Targets
-        </Button>
-      </>
+        </Box>
+      )}
+    </>
   );
 }
